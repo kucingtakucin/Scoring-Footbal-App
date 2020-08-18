78eb68b85d016ddf117da6171a703b1ffb9b2c81
Adam Arthur Faizal <adam.faizal.af6@gmail.com>
2020-08-18 15:38:25 +0700

    Menambahkan app bar dan bottom navigation

--- app/src/main/java/com/android/scoringapp/MainActivity.kt
+++ app/src/main/java/com/android/scoringapp/MainActivity.kt
@@ -2,10 +2,35 @@ package com.android.scoringapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.android.scoringapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var dataBinding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        dataBinding = DataBindingUtil.setContentView(this@MainActivity, R.layout.activity_main)
+        dataBinding.apply {
+            setSupportActionBar(toolbarMain)
+            navController = findNavController(R.id.nav_host_fragment)
+            appBarConfiguration = AppBarConfiguration(navController.graph)
+            toolbarMain.setupWithNavController(navController, appBarConfiguration)
+            bottomMavigation.setupWithNavController(navController)
+            setupActionBarWithNavController(navController, appBarConfiguration)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) or super.onSupportNavigateUp()
     }
 }
