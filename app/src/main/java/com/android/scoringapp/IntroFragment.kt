41500a93fd71e1adcb5c5ce475a39628ea5ee79c
Adam Arthur Faizal <adam.faizal.af6@gmail.com>
2020-08-19 16:47:28 +0700

    Memperbaiki UI dan koneksi antar fragment

--- app/src/main/java/com/android/scoringapp/IntroFragment.kt
+++ app/src/main/java/com/android/scoringapp/IntroFragment.kt
@@ -1,11 +1,12 @@
 package com.android.scoringapp
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.android.scoringapp.databinding.FragmentIntroBinding
 
 /**
@@ -13,6 +14,7 @@ import com.android.scoringapp.databinding.FragmentIntroBinding
  */
 class IntroFragment : Fragment() {
     private lateinit var dataBinding: FragmentIntroBinding
+    private lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,8 +22,21 @@ class IntroFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         dataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_intro, container, false)
+        setHasOptionsMenu(true)
         dataBinding.apply {
             return root
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.overflow_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return view?.findNavController().let {
+            NavigationUI.onNavDestinationSelected(item, it!!)
+        } or super.onOptionsItemSelected(item)
+
     }
 }
