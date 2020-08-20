8af28c4c6ccb7b88d50e28f78e943b8df34db5af
Adam Arthur Faizal <adam.faizal.af6@gmail.com>
2020-08-20 14:37:03 +0700

    Menyelesaikan semua fragment

--- app/src/main/java/com/android/scoringapp/FinishFragment.kt
+++ app/src/main/java/com/android/scoringapp/FinishFragment.kt
@@ -1,21 +1,32 @@
 package com.android.scoringapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.android.scoringapp.databinding.FragmentFinishBinding
 
 /**
  * A simple [Fragment] subclass.
  */
 class FinishFragment : Fragment() {
+    private lateinit var dataBinding: FragmentFinishBinding
+    private lateinit var args: FinishFragmentArgs
 
+    @SuppressLint("SetTextI18n")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_finish, container, false)
+        dataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_finish, container, false)
+        args = arguments?.let { FinishFragmentArgs.fromBundle(it) }!!
+        dataBinding.apply {
+            resultText.text = "${args.name} menang dengan score ${args.score}!!!"
+            return root
+        }
     }
 }
